66845d967edfd691172a3b7338215f3e921a4ee2
kevinzhan(湛杨梦晓) <kevinzhan@tencent.com>
2018-12-18 17:12:44 +0800

    重构抽出RainDropWidget

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
 import 'rain_drop.dart';
+
 void main() {
   debugPaintSizeEnabled = true;
   runApp(MyApp());
@@ -30,57 +31,18 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin{
-
-  List<RainDropDrawer> rainList;
-  final GlobalKey paintKey = GlobalKey();
-  AnimationController animation;
-
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
   @override
   void initState() {
     super.initState();
-    rainList = List();
-    animation = new AnimationController(
-      // 这个动画应该持续的时间长短
-        duration: const Duration(milliseconds: 500),
-        vsync: this
-    )..addListener(() {
-      if(rainList.isEmpty) {
-        animation.stop();
-      }
-      setState(() {
-
-      });
-    });
-
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Container(
-          color: Colors.lightBlueAccent,
-          child: GestureDetector(
-            key: paintKey,
-            onTapUp: (TapUpDetails tapUp) {
-              RenderBox getBox = paintKey.currentContext.findRenderObject();
-              var localOffset = getBox.globalToLocal(tapUp.globalPosition);
-
-              var rainDrop = RainDropDrawer(localOffset.dx, localOffset.dy);
-              rainList.add(rainDrop);
-              animation.repeat();
-            },
-            child: CustomPaint(
-              painter: RainDrop(rainList),
-              size: Size(300, 300),
-            ),
-          ),
-        )
+        appBar: AppBar(
+          title: Text(widget.title),
         ),
-      );
+        body: Center(child: RainDropWidget(width: 300, height: 400,)));
   }
 }
--- lib/rain_drop.dart
+++ lib/rain_drop.dart
@@ -1,10 +1,76 @@
 import 'package:flutter/rendering.dart';
+import 'package:flutter/material.dart';
+
+class RainDropWidget extends StatefulWidget {
+
+  RainDropWidget({Key key, this.width, this.height}) : super(key: key);
+
+  final double width;
+  final double height;
+
+  @override
+  State<StatefulWidget> createState() {
+    return RainDropState(width, height);
+  }
+}
+
+class RainDropState extends State<RainDropWidget>
+    with TickerProviderStateMixin {
+  List<RainDropDrawer> _rainList;
+  AnimationController _animation;
+  double _width = 300;
+  double _height = 300;
+
+  RainDropState(double width, double height) {
+    _width = width ?? _width;
+    _height = height ?? _height;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _rainList = List();
+    _animation = new AnimationController(
+      // 因为是repeat的，这里的duration其实不care
+        duration: const Duration(milliseconds: 200),
+        vsync: this)
+      ..addListener(() {
+        if (_rainList.isEmpty) {
+          _animation.stop();
+        }
+        setState(() {});
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.lightBlueAccent,
+      width: _width,
+      height: _height,
+      child: GestureDetector(
+        onTapUp: (TapUpDetails tapUp) {
+          RenderBox getBox = context.findRenderObject();
+          var localOffset = getBox.globalToLocal(tapUp.globalPosition);
+
+          var rainDrop = RainDropDrawer(localOffset.dx, localOffset.dy);
+          _rainList.add(rainDrop);
+          _animation.repeat();
+        },
+        child: CustomPaint(
+          painter: RainDrop(_rainList),
+        ),
+      ),
+    );
+  }
+}
 
 class RainDrop extends CustomPainter {
   RainDrop(this.rainList);
 
   List<RainDropDrawer> rainList = List();
-  Paint  _paint = new Paint()..style = PaintingStyle.stroke;
+  Paint _paint = new Paint()..style = PaintingStyle.stroke;
+
   @override
   void paint(Canvas canvas, Size size) {
     rainList.forEach((item) {
@@ -33,11 +99,10 @@ class RainDropDrawer {
     double opt = (MAX_RADIUS - radius) / MAX_RADIUS;
     paint.color = Color.fromRGBO(0, 0, 0, opt);
     canvas.drawCircle(Offset(posX, posY), radius, paint);
-    radius += 1;
+    radius += 0.5;
   }
 
   bool isValid() {
     return radius < MAX_RADIUS;
   }
-
 }
